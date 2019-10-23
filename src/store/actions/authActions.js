f542b2e260a420698dca3c9be20b10b3b4d1edf1
demetr <dmitriev@joomdesign.ru>
2019-10-24 00:16:20 +0300

    Показ ошибки при авторизации.

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,4 +1,5 @@
-import {AUTH_USER_REQUEST, AUTH_USER_SUCCESS, FETCH_USER_SUCCESS, LOGOUT_USER} from '../types';
+import {stopSubmit} from 'redux-form';
+import {AUTH_USER_FAILURE, AUTH_USER_REQUEST, AUTH_USER_SUCCESS, FETCH_USER_SUCCESS, LOGOUT_USER} from '../types';
 import {authAPI} from '../../api/api';
 import jwtDecode from 'jwt-decode';
 import setAuthToken from '../../utils/set-auth-token';
@@ -42,8 +43,9 @@ export const loginUser = userData => async dispatch => {
     dispatch(userLoaded(jwtDecode(token)));
 
   } catch (e) {
-    console.log(e);
-    dispatch(userLoaded({}));
+    dispatch(stopSubmit('login',
+        {_error: 'Ошибка какая-то...'}
+    ));
   }
 };
 
