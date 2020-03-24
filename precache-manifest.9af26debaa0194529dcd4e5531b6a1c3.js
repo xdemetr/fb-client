d2c993ddddbcfe14dadfe18da94e0c63faa335fc
demetr <dmitriev@joomdesign.ru>
2020-03-24 22:45:45 +0300

    Updates

---
+++ precache-manifest.9af26debaa0194529dcd4e5531b6a1c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb452e3eb5adcb0674a17a8292cf535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a93dfa9df8ac8cd220",
+    "url": "/static/css/main.9ecbf1b0.chunk.css"
+  },
+  {
+    "revision": "a1af00e300eec16bc663",
+    "url": "/static/js/2.8b4b2ea9.chunk.js"
+  },
+  {
+    "revision": "6b41b2197af5d3e6053a",
+    "url": "/static/js/3.0147ae94.chunk.js"
+  },
+  {
+    "revision": "25bb5031af01e328517b",
+    "url": "/static/js/4.42d96a42.chunk.js"
+  },
+  {
+    "revision": "80153a3cba55d4cd1f86",
+    "url": "/static/js/5.320288d8.chunk.js"
+  },
+  {
+    "revision": "b8275948a0bf2505a4aa",
+    "url": "/static/js/6.e52ed013.chunk.js"
+  },
+  {
+    "revision": "f3a93dfa9df8ac8cd220",
+    "url": "/static/js/main.16e2df8c.chunk.js"
+  },
+  {
+    "revision": "677f036e684ba2222ad8",
+    "url": "/static/js/runtime-main.7373531e.js"
+  },
+  {
+    "revision": "761182ff79caf365ad85fcb8226e3bbf",
+    "url": "/static/media/logo.761182ff.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fb-client/precache-manifest.488ecf2d2b66299603ecb16b8afa7bc4.js"
+  "/precache-manifest.9af26debaa0194529dcd4e5531b6a1c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fb-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
